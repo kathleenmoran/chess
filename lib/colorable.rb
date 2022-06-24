d26acfe5d14a976e64388e2a68019b87216b07e6
Kathleen Moran <kathleenmoran10@gmail.com>
2022-06-24 15:53:03 -0400

    Allow player to select alphanumeric coordinates and convert them to numeric coordinates

--- lib/colorable.rb
+++ lib/colorable.rb
@@ -2,7 +2,7 @@
 
 # ability to color text using ANSI escape codes
 module Colorable
-  COLOR_CODES = { black: 0, dark_green: 22, light_green: 150, neon_green: 10, white: 231, yellow: 220 }.freeze
+  COLOR_CODES = { black: 0, dark_green: 22, light_green: 150, neon_green: 10, red: 9, white: 231, yellow: 220 }.freeze
 
   def color_text(text, color)
     "\033[38;5;#{COLOR_CODES[color]}m#{text}\033[0m"
@@ -18,5 +18,19 @@ module Colorable
     else
       :light_green
     end
+  end
+
+  def prompt_piece_selection(player)
+    puts "#{player}, enter the coordinate of the piece you would like to move (e.g., A1):"
+    gets.chomp
+  end
+
+  def prompt_move_selection(player)
+    puts "#{player}, enter the coordinate of the square you would like the selected piece to move to (e.g., A1):"
+    gets.chomp
+  end
+
+  def print_invalid_coord_message(invalid_coord)
+    puts color_text("'#{invalid_coord}' is not a valid coordinate.\n", :red)
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
+require_relative 'coordinate'
+require_relative 'colorable'
+
 # a player of a chess game
 class Player
+  include Colorable
   def initialize(color)
     @color = color
+  end
+
+  def to_s
+    @color.to_s.capitalize
   end
 
   def white?
@@ -17,4 +25,32 @@ class Player
   def own_piece_at_square?(square)
     (white? && square.occupied_by_white?) || (black? && square.occupied_by_black?)
   end
+
+  def select_start_square
+    select_square { prompt_piece_selection(self) }
+  end
+
+  def select_end_square
+    select_square { prompt_move_selection(self) }
+  end
+
+  def select_square(&block)
+    alpha_coord = yield
+    if valid_alpha_coord?(alpha_coord)
+      alpha_to_numeric_coord(alpha_coord)
+    else
+      print_invalid_coord_message(alpha_coord)
+      select_square(&block)
+    end
+  end
+
+  def valid_alpha_coord?(alpha_coord)
+    alpha_coord.length == 2 && alpha_coord[0].match?(/[A-Ha-h]/) && alpha_coord[1].match?(/[1-8]/)
+  end
+
+  def alpha_to_numeric_coord(alpha_coord)
+    Coordinate.new(alpha_coord[0].upcase.ord - 65, alpha_coord[1].to_i - 1)
+  end
 end
+
+Player.new(:black).select_end_square
